cdb451c959098bf40fef095e0f688a2e5bc74c5e
valdljr <valdir.lago@bling.com.br>
2020-09-01 20:59:27 -0300

    feat: add connection to database

--- src/server.ts
+++ src/server.ts
@@ -3,27 +3,37 @@ import { Server } from '@overnightjs/core'
 import bodyParser = require('body-parser')
 import { ForecastController } from './controllers/forecast'
 import { Application } from 'express'
+import * as database from '@src/database'
 
 export class SetupServer extends Server {
-  constructor(private port = 3000) {
-    super()
-  }
+    constructor(private port = 3000) {
+        super()
+    }
 
-  public init(): void {
-    this.setupExpress()
-    this.setupController()
-  }
+    public async init(): Promise<void> {
+        this.setupExpress()
+        this.setupController()
+        await this.databaseSetup()
+    }
 
-  private setupExpress(): void {
-    this.app.use(bodyParser.json())
-  }
+    private setupExpress(): void {
+        this.app.use(bodyParser.json())
+    }
 
-  private setupController(): void {
-    const forecastController = new ForecastController()
-    this.addControllers([forecastController])
-  }
+    private setupController(): void {
+        const forecastController = new ForecastController()
+        this.addControllers([forecastController])
+    }
 
-  public getApp(): Application {
-    return this.app
-  }
+    private async databaseSetup(): Promise<void> {
+        await database.connect()
+    }
+
+    public async close(): Promise<void> {
+        await database.close()
+    }
+
+    public getApp(): Application {
+        return this.app
+    }
 }
