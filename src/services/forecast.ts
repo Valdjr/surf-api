736d5f9e39be61b9c245f9c75ff020b1d56ab43e
valdljr <valdir.lago@bling.com.br>
2020-09-01 21:56:49 -0300

    feat: add Beach model and tests

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,20 +1,6 @@
 import { StormGlass, ForecastPoint } from '@src/clients/stormGlass'
 import { InternalError } from '@src/util/errors/internal-error'
-
-export enum BeachPosition {
-    S = 'S',
-    E = 'E',
-    W = 'W',
-    N = 'N',
-}
-
-export interface Beach {
-    lat: number
-    lng: number
-    name: string
-    user: string
-    position: BeachPosition
-}
+import { Beach } from '@src/models/beach'
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
