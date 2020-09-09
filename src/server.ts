3c3cf4934125f1d9b320d7a2a802b255467c4b4e
valdljr <valdir.lago@bling.com.br>
2020-09-09 20:21:12 -0300

    docs: add openapi documentation and validation

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
 import './util/module-alias'
 import { Server } from '@overnightjs/core'
+import { Application } from 'express'
+
 import bodyParser = require('body-parser')
 import expressPino from 'express-pino-logger'
+import logger from './logger'
 import cors from 'cors'
-import { ForecastController } from './controllers/forecast'
-import { Application } from 'express'
+
+import swaggerUi from 'swagger-ui-express'
+import apiSchema from './api.schema.json'
+import { OpenApiValidator } from 'express-openapi-validator'
+import { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types'
+
 import * as database from '@src/database'
+
+import { ForecastController } from './controllers/forecast'
 import { BeachesController } from './controllers/beaches'
 import { UsersController } from './controllers/users'
-import logger from './logger'
+import { apiErrorValidator } from './middlewares/api-error-validator'
 
 export class SetupServer extends Server {
     constructor(private port = 3000) {
@@ -17,8 +26,10 @@ export class SetupServer extends Server {
 
     public async init(): Promise<void> {
         this.setupExpress()
+        await this.docsSetup()
         this.setupController()
         await this.databaseSetup()
+        this.setupErrorHandlers()
     }
 
     private setupExpress(): void {
@@ -31,6 +42,10 @@ export class SetupServer extends Server {
         this.app.use(cors({ origin: '*' }))
     }
 
+    private setupErrorHandlers(): void {
+        this.app.use(apiErrorValidator)
+    }
+
     private setupController(): void {
         const forecastController = new ForecastController()
         const beachesController = new BeachesController()
@@ -40,6 +55,15 @@ export class SetupServer extends Server {
             beachesController,
             usersController,
         ])
+    }
+
+    private async docsSetup(): Promise<void> {
+        this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiSchema))
+        await new OpenApiValidator({
+            apiSpec: apiSchema as OpenAPIV3.Document,
+            validateRequests: true,
+            validateResponses: true,
+        }).install(this.app)
     }
 
     private async databaseSetup(): Promise<void> {
